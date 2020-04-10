163a631eed0c1c001b5b72a3441fcfcef114f6e2
Vahanbarsamian <vahanbarsamian>
2020-04-10 17:48:50 +0200

    end family entity
    Signed-off-by: Vahanbarsamian <vahanbarsamian>

--- src/Controller/FamilleController.php
+++ src/Controller/FamilleController.php
@@ -3,21 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\Famille;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\FamilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class FamilleController extends AbstractController
 {
     /**
      * @Route("/familles", name="list_famille")
      */
-    public function listAction()
+    public function listAction(FamilleRepository $repository)
     {
-        $repository = $this->getDoctrine()->getRepository(Famille::class);
-        $familles =  $repository->findAll();
+        $familles = $repository->findAll();
         return $this->render('famille/familles.html.twig', [
-            'titre' => 'Liste des familles',
-            "familles" => $familles
+            'titre'    => 'Liste des familles',
+            "familles" => $familles,
+        ]);
+    }
+    
+    /**
+     * @Route("/famille/{id}", name="get_famille")
+     */
+    public function getAction(Famille $famille)
+    {
+        return $this->render('famille/get_famille.html.twig', [
+            'titre'    => 'Animaux de la famille : '.$famille->getLibelle(),
+            "famille" => $famille,
         ]);
     }
 }
